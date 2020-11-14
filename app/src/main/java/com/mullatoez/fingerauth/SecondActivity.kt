b3e3370234db42f3fdfd0843066c17113f672378
Mullatoez <kimanthikevin76@gmail.com>
2020-11-14 22:48:11 +0300

    fingerprint auth

---
+++ app/src/main/java/com/mullatoez/fingerauth/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.mullatoez.fingerauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
